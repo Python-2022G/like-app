c67643975862d6906eec5a992828b1ee218d9901
djumanov <diyorbekjumanov1120@gmail.com>
2023-02-22 17:56:02 +0500

    done

--- app.py
+++ app.py
@@ -21,11 +21,11 @@ def home():
 def like():
     if request.method == 'POST':
         data = request.get_json(force=True) # get data from request body
-        photo_id = data['photo_id'] # get photo id
+        doc_id = data['doc_id'] # get photo id
         chat_id = data['chat_id'] # get chat id
 
         # like photo
-        if add_like(photo_id, chat_id):
+        if add_like(doc_id, chat_id):
             return {'status': 200}
         return {'status': 400}
 
@@ -34,19 +34,19 @@ def like():
 def dislike():
     if request.method == 'POST':
         data = request.get_json(force=True) # get data from request body
-        photo_id = data['photo_id'] # get photo id
+        doc_id = data['doc_id'] # get photo id
         chat_id = data['chat_id'] # get chat id
 
         # like photo
-        if add_dislike(photo_id, chat_id):
+        if add_dislike(doc_id, chat_id):
             return {'status': 200}
         return {'status': 400}
     
 
-@app.route('/api/get-data/<photo_id>')
-def get_data_view(photo_id: str):
+@app.route('/api/get-data/<doc_id>')
+def get_data_view(doc_id: str):
     '''get data about img'''
-    data = get_data(photo_id)
+    data = get_data(doc_id)
     if data:
         return data
     return {'status': 400}
